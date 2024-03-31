5c3f30ea9a9d203775972731713f944bf0ef6867
gossdouglas <gossdouglas@yahoo.com>
2024-03-31 10:18:47 -0500

    initial commit.

---
+++ duto.api.service/Service1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.IO;
+
+namespace dto.api.service
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Debug.WriteLine("Service began at: " + DateTime.Now);
+            //timer.Elapsed = 
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
